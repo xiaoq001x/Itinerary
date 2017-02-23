901feb2cd86d03a25060194f8f3cab314f778698
肖强 <1490075359@qq.com>
2017-02-23 18:05:42 +0800

    添加注解

---
+++ src/main/java/com/itinerary/servlet/test.java
@@ -0,0 +1,35 @@
+package com.itinerary.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @author 肖强
+ * @Time 2017/2/23 10:56
+ */
+public class test implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
